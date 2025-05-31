b2b9f64052b5c5c98150843dc4ecb8bd57483e91
Houston Perrett <hp@houstonp.com>
2025-05-31 12:27:38 -0500

    add day count convention modules

--- ak-core/src/day_count.rs
+++ ak-core/src/day_count.rs
@@ -1,6 +1,15 @@
 use jiff::civil::Date;
 mod actual_360;
+mod actual_36525;
+mod actual_365_fixed;
+mod bus_252;
+mod isda_1_1;
+mod isda_30_360;
 mod isda_actual_actual;
+mod nl_365;
+mod psa_30_360_eom;
+mod sia_30_360_eom;
+mod sia_30_360_neom;
 
 trait DayCount {
     fn year_diff(&self, start: Date, end: Date) -> f64;
