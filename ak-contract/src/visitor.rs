eea23695952326bca1c6359d62fd0b244bbb243e
Houston Perrett <hp@houstonp.com>
2025-06-15 12:37:25 -0500

    add ConstVisitor

--- ak-contract/src/visitor.rs
+++ ak-contract/src/visitor.rs
@@ -9,3 +9,10 @@ pub trait Visitor {
     /// Called after all children have been visited.
     fn post_visit(&mut self, node: &Node);
 }
+
+pub trait ConstVisitor {
+    /// Called before recursing into children.
+    fn pre_visit(&self, node: &Node);
+    /// Called after all children have been visited.
+    fn post_visit(&self, node: &Node);
+}
