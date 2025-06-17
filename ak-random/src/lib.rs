960fc950f81f66413d5021c5931f14e2f80dc818
Houston Perrett <hp@houstonp.com>
2025-06-16 23:50:15 -0500

    clean up cargo boilerplate

--- ak-random/src/lib.rs
+++ ak-random/src/lib.rs
@@ -119,18 +119,3 @@ impl Mrg32k3a {
         }
     }
 }
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
